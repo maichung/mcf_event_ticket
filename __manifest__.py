04a3824ddc2c61f9152bfa05512b4ae2897ffbbf
maichung <maichunglk@gmail.com>
2025-09-25 13:14:51 +0700

    Create __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'MCF Event Ticket',
+    'version': '1.0',
+    'summary': 'Custom Full Page Event Ticket for MCFEED',
+    'author': 'MOSCOW FEED',
+    'category': 'Event',
+    'depends': ['event'],
+    'data': [
+        'reports/event_ticket_template.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
